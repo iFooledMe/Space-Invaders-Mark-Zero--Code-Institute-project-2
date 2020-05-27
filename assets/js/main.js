e4b3ba767b404b3e65d74a85598089b5a133d66e
iFooledMe <60869593+iFooledMe@users.noreply.github.com>
2020-05-27 17:45:34 +0200

    Refactor:  Some Refactor and reorganization to the js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
-// ==== Global variables ====================================================================
-var paused = true;
-
-// ==== Images pre-load =====================================================================
+// ==========================================================================================
+// ==== I M A G E S   P R E - L O A D =======================================================
 var Images = {};
 
 function loadImages(list){
@@ -10,7 +8,6 @@ function loadImages(list){
     for(var i = 0; i < list.length; i++){
         var img = new Image();
         Images[list[i].name] = img;
-        
         img.onload = function(){
             total++;
             if(total == list.length){
@@ -21,47 +18,88 @@ function loadImages(list){
     }
 }
 
+// .... Images List ............................................................................
 loadImages([{
-    name: "player_starship",
-    url: "assets/images/spaceship-200x100.png",
+    name: "player_starship_small",
+    url: "assets/images/spaceship-100x50.png",
 },{
-    name: "weirdCircles",
-    url: "http://www.nasa.gov/images/content/498887main_Fermi_bubble_art_no_labels.jpg"
+    name: "player_starship_medium",
+    url: "assets/images/spaceship-200x100.png"
 }]);
 
-// ==== START =====================================================================================
-function start(){
-  var ctx = $("canvas")[0].getContext("2d");
-  ctx.drawImage(Images["player_starship"], 100, 300, 60, 40);
+// =============================================================================================
+// ==== G L O B A L   V A R I A B E L S ========================================================
+
+// .... GAME ...................................................................................
+var ctx = null; //Canvas Context
+var paused = true;
+
+// .... PLAYER .................................................................................
+var playerStartPosX = 100; //Horizontal axis
+var playerStartPosY = 300; //Vertical axis
+var playerStartSizeW =  60; //Width
+var playerStartSizeH =  40; //Height
+
+var playerCurrentPosX = playerStartPosX;
+var playerCurrentPosY = playerStartPosY;
+
+// =============================================================================================
+// ==== G A M E - O B J E C T S ================================================================
+
+var Game = {
+    canvas : null,
+    context : null,
+    getMyContext : function (canvasId, width, height) {
+        this.canvas = document.getElementById(canvasId);
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.context = this.canvas.getContext("2d");
+        return this.context;
+    }
 }
 
-//some jQuery stuff that is not related
-$(".game-info-bar button").click(function(){
-    
-    if (paused){
-        paused = false;
-        $(".game-info-bar button").text("Game Started!");
-        start();
-    }
-    else if (!paused) {
-        paused = true;
-        $(".game-info-bar button").text("Game Paused!");
-        pause(); 
-        /* TODO: Do pause function */ 
-        /* TODO: Connect Pause Button */ 
-    }
-    else {
-        alert("Error! Game is neither started or paused!")
-    }
-});
+// =============================================================================================
+// ==== START =========?========================================================================
+
+$(document).ready(function() {
+
+    start();
+})
+
+function start() {
+
+    //Create Game Context Object in Canvas 
+    ctx = Game.getMyContext("canvas-1", 1000, 700);
+
+    loadResources(); //Images and resources on the screen
+    setControls(); //Key Commands
+
+}
+
+
+// **** LOAD RESOURCES ****************************************************************************
+function loadResources() {
+
+    drawPlayerStarShip();
+
+}
+
+// .......... Player ..........
+function drawPlayerStarShip() {
+    ctx.drawImage(Images["player_starship_small"], 50, 300);
+}
+
+
+
+// .......... Enemies ..........
 
 
 
 
+// **** SET CONTROLS ****************************************************************************
+
+function setControls() {
 
 
 
-
-
-
-
+}
---
+++ assets/js/main_old.js
@@ -0,0 +1,131 @@
+// :::: GLOBAL VARIABLES ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// .... Game ................................................................................
+var ctx;
+var paused = true;
+
+// .... Player ..............................................................................
+var playerStartPosX = 100; //Horizontal axis
+var playerStartPosY = 300; //Vertical axis
+var playerStartSizeW =  60; //Width
+var playerStartSizeH =  40; //Height
+
+var playerCurrentPosX = playerStartPosX;
+var playerCurrentPosY = playerStartPosY;
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// ==========================================================================================
+// ==== IMAGES PRE-LOAD =====================================================================
+var Images = {};
+
+function loadImages(list){
+    var total = 0;
+    $("span").text("Loading...");
+    for(var i = 0; i < list.length; i++){
+        var img = new Image();
+        Images[list[i].name] = img;
+        
+        img.onload = function(){
+            total++;
+            if(total == list.length){
+                $("span").text("Loaded.");
+            }
+        };
+        img.src = list[i].url;
+    }
+}
+
+// .... List of Images ..........................................................................
+loadImages([{
+    name: "player_starship",
+    url: "assets/images/spaceship-200x100.png",
+},{
+    name: "weirdCircles",
+    url: "http://www.nasa.gov/images/content/498887main_Fermi_bubble_art_no_labels.jpg"
+}]);
+
+
+// ================================================================================================
+// ==== START =====================================================================================
+function start(){
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    createPlayer(ctx);
+}
+
+// .... Start / Pause ..............................................................................
+$(".game-info-bar button").click(function(){
+    
+    if (paused){
+        paused = false;
+        $(".game-info-bar button").text("Game Started!");
+        start();
+    }
+    else if (!paused) {
+        paused = true;
+        $(".game-info-bar button").text("Game Paused!");
+        pause(); 
+        /* TODO: Do pause function */ 
+        /* TODO: Connect Pause Button */ 
+    }
+    else {
+        alert("Error! Game is neither started or paused!")
+    }
+});
+
+
+// ================================================================================================
+// ==== PLAYER ====================================================================================
+
+
+/* Create player ship in canvas */
+function createPlayer(ctx) {
+    ctx.drawImage(Images["player_starship"], playerStartPosX, playerStartPosY, playerStartSizeW, playerStartSizeH);
+};
+
+
+/* Check for keystrokes */
+
+    document.onkeydown = function(event) {
+        var direction;
+        
+        switch (event.keyCode) {
+           case 37:
+              break;
+           case 38:
+            direction = "up";
+            movePlayer(direction);
+              break;
+           case 39:
+              break;
+           case 40:
+            direction = "down";
+            movePlayer(direction);
+              break;
+        }
+    };
+
+/* Move player */
+    function movePlayer(direction) {
+        
+        if (direction == "up") {
+
+            alert("UUUUPPPPP!!!!!" + direction);
+
+        }
+        else if (direction == "down") {
+            alert("DOOOOOWWWN!!!!" + direction);
+        }
+
+    }
+
+
+
+
+
+
+
+
+
