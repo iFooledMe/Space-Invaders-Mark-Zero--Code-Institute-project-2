c32c3b7a53e51d54fb6e3360757f6582b3d331d5
iFooledMe <60869593+iFooledMe@users.noreply.github.com>
2020-05-28 16:57:56 +0200

    Fix: Fixed issue with Player image not showing in canvas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,18 @@
 // ==========================================================================================
 // ==== I M A G E S   P R E - L O A D =======================================================
-
 var Images = {};
 
 function loadImages(list){
     var total = 0;
+    $("span").text("Loading...");
     for(var i = 0; i < list.length; i++){
         var img = new Image();
         Images[list[i].name] = img;
+        
         img.onload = function(){
             total++;
             if(total == list.length){
+                $("span").text("Loaded.");
             }
         };
         img.src = list[i].url;
@@ -18,81 +20,91 @@ function loadImages(list){
 }
 
 // .... Images List ............................................................................
-loadImages([{
-    name: "player_starship_small",
-    url: "assets/images/spaceship2_120x60.png",
-},{
-    name: "player_starship_medium",
-    url: "assets/images/spaceship2_120x60.png"
-}]);
+loadImages ([   
+    {   name: "player_starship_small",
+        url: "assets/images/spaceship2_120x60.png"      },
+    {   name: "player_starship_medium",
+        url: "assets/images/spaceship2_120x60.png"      },
+    {   name: "bg-planet-1",
+        url: "assets/images/bg-planet-3840x2160.jpg"    }
+]);
 
 // =============================================================================================
 // ==== G L O B A L   V A R I A B L E S ========================================================
-
-
+var Game;
+var ctx;
+var player;
 
 
 // =============================================================================================
 // ==== G A M E - O B J E C T S ================================================================
 
 // .... GAME ...................................................................................
-function Game (canvasId, width, height) {
-    this.canvas = document.getElementById(canvasId);
-    this.canvas.width = width;
-    this.canvas.height = height;
-    this.paused = true; //Game paused at start
-    this.context = this.canvas.getContext("2d");
+var Game = {
+    canvas : null,
+    context : null,
+    create : function(canvasId) { 
+        this.canvas = document.getElementById(canvasId);
+        this.context = this.canvas.getContext("2d");
+        return this.context;
+    },
+    isPaused : true, //Game paused at load
 };
 
 // .... PLAYER ..................................................................................
-function Player(Game, name) {
-    this.game = Game;
+function Player(ctx,name) {
     this.name = name;
-    this.playerPosX = 100; //Horizontal axis at start
-    this.playerPosY = 270; //Vertical axis at start
-    this.playerSizeW =  120; //Width at start
-    this.playerSizeH =  60; //Height at start
+    this.posX = 100; //Horizontal axis at start
+    this.posY = 270; //Vertical axis at start
+    this.sizeW =  120; //Width at start
+    this.sizeH =  60; //Height at start
     this.playerDrawPosition = function() {
-        return game.context.drawImage(Images["player_starship_small"], 
-        this.playerPosX, 
-        this.playerPosY, 
-        this.playerSizeW, 
-        this.playerSizeH);
+        ctx.drawImage(Images["player_starship_small"], this.posX, this.posY, this.sizeW, this.sizeH);
     };
 };
 
-
-
-// =============================================================================================
-// ==== S T A R T =========?====================================================================
+// ===============================================================================================
+// ==== S T A R T ================================================================================
 
 $(document).ready(function() {
+    
+    //Create Canvas
+    ctx = Game.create("canvas-1");
+    
 
-    start();
+      
+
+    
+
+
+
+    start(ctx);
 })
 
-function start() {
+// ==== GAME ENTRY ================================================================================
+function start(ctx) {
 
-    var newGame = new Game("canvas", 300, 300);
 
-    var player = new Player(newGame, "MT");
-    player.playerDrawPosition();
 
-    loadResources(); //Images and resources on the screen
+    loadResources(ctx); //Images and resources on the screen*/
     setControls(); //Key Commands
 
+    
+    
 }
 
-// .... Start / Pause .............................................................................
+// ===============================================================================================
+// ==== S T A R T  / P A U S E ===================================================================
+
 $(".game-info-bar button").click(function(){
     
-    if (gamePaused){
-        paused = false;
+    if (Game.isPaused){
+        Game.isPaused = false;
         $(".game-info-bar button").text("Game Started!");
-        start();
+        unPause();
     }
-    else if (!paused) {
-        paused = true;
+    else if (!Game.isPaused) {
+        Game.isPaused = true;
         $(".game-info-bar button").text("Game Paused!");
         pause(); 
         /* TODO: Do pause function */ 
@@ -103,16 +115,31 @@ $(".game-info-bar button").click(function(){
     }
 });
 
-
-// **** LOAD RESOURCES ****************************************************************************
-function loadResources() {
-
-    
+function unPause() {
 
 }
 
-// .......... Player ..........
+function pause() {
+    
+}
 
+
+// ===============================================================================================
+// ==== G A M E - R U N N I N G ==================================================================
+
+
+// **** LOAD RESOURCES ****************************************************************************
+function loadResources(ctx) {
+
+    inGame_player(ctx); //Creates the player ship, score and other statuses
+}
+
+// .......... Player ...........
+function inGame_player(ctx) {
+    player = new Player(ctx, "Mark");
+    player.playerDrawPosition();
+    
+};
 
 
 // .......... Enemies ..........
@@ -128,12 +155,9 @@ function setControls() {
     document.addEventListener('keydown', logKey);
 
     function logKey(e) {
-        log.textContent += ` ${e.code}`; /* TODO: Just for test, remove later!!!  */
+        log.textContent = ` ${e.code}`; /* TODO: Just for test, remove later!!!  */
         var keyStroke = ` ${e.code}`;
-       alert(keyStroke);
         playerActions(keyStroke); 
-        
-
     }
 
     function playerActions(keyStroke) {       
@@ -162,7 +186,6 @@ function setControls() {
 
     }
 
-
+}
 
     
-}
